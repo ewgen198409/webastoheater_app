a9e80f405d37932e3b2072fb1adf143998fd6680
ewgen198409 <ewgeniy198409@gmail.com>
2025-10-03 23:08:33 +0600

    new

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,9 @@ signingConfigs {
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.preference:preference-ktx:1.2.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("androidx.cardview:cardview:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
